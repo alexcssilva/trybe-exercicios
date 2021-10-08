8b80f0d8fc88819cd8eba9b6adb99a1813249634
Alex Silva <alexcssilva@gmail.com>
2021-10-08 19:35:27 -0300

    Exercicios 2 concluido

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -21,3 +21,12 @@ console.log(modulo);
 
 // Exercícios 2 - Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
 
+const a = 12;
+const b = 8;
+
+if (a > b) {
+  console.log("Variável 'a' é maior que 'b'");
+}
+else {
+  console.log("Variável 'b' é maior que 'a'")
+}
