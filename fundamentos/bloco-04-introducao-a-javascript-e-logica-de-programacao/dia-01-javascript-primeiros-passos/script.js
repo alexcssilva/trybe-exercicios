75d3915d00036a16fa48dada35f239d482ae5c27
Alex Silva <alexcssilva@gmail.com>
2021-10-08 19:34:08 -0300

    Exercicios 1 concluido

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -0,0 +1,23 @@
+
+// Exercícios 1 - Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+
+const a = 5;
+const b = 3;
+
+const adicao = a + b;
+console.log(adicao);
+
+const subtracao = a - b;
+console.log(subtracao);
+
+const multiplicacao = a * b;
+console.log(multiplicacao);
+
+const divisao = a / b;
+console.log(divisao);
+
+const modulo = a % b;
+console.log(modulo);
+
+// Exercícios 2 - Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+
