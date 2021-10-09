d15081dc2c91481b5227aa3382b90bb614c0f212
Alex Silva <alexcssilva@gmail.com>
2021-10-08 23:00:53 -0300

    Exercicio 4 concluido

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -46,3 +46,17 @@ else if (b > a && b > a) {
 else {
   console.log("Variável C é maior que A e B");
 }
+
+// Exercícios 4 - Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+const valor= 0;
+
+if (valor > 0) {
+  console.log("Positivo");
+}
+else if (valor == 0){
+  console.log("Zero");
+}
+else {
+  console.log("Negativo");
+}
