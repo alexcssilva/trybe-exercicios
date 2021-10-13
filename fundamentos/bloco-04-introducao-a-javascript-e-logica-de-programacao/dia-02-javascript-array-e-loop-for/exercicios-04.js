c5da95e0ad6dea48cf96c345798d4b7eb26c57a4
Alex Silva <alexcssilva@gmail.com>
2021-10-13 20:15:21 -0300

    Exercicios 06 concluido

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios-04.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios-04.js
@@ -3,8 +3,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
 
-for (let i = 0; i < numbers.length; i++){
-  sum += numbers[i];
+for (let i = 0; i < numbers.length; i += 1){
+  (sum += numbers[i]);
 }
 
 let res = sum / numbers.length;
