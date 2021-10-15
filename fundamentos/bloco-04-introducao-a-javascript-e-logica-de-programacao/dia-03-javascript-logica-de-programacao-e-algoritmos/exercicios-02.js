63db8fbd304f40653c66f2a8a74bc0981ef45c41
Alex Silva <alexcssilva@gmail.com>
2021-10-15 01:02:56 -0300

    Exercicios 01 concluido

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-02.js
@@ -0,0 +1,12 @@
+
+let letter = 'string';
+let res = '';
+
+for (let index = 0; index < letter.length; index++) {
+  letter[2] = 'R';
+  console.log(index, ' , ' , letter[index] )
+  res = letter[2];
+  
+}
+
+console.log(letter)
