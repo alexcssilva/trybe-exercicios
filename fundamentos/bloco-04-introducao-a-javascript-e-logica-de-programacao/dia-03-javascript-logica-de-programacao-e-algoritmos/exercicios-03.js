cd5ea13cb4cc675808f876a7a8b480e56054131f
Alex Silva <alexcssilva@gmail.com>
2021-11-03 18:53:30 -0300

    Atualizacao

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-03.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-03.js
@@ -1,24 +1,24 @@
-// Exercicios 3 - Agora inverta o lado do triângulo. Por exemplo:
+// Exercicios 2 - Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
 
 // n = 5
 
-//     *
-//    **
-//   ***
-//  ****
+// *
+// **
+// ***
+// ****
 // *****
 
 let n = 5;
-let asterisk = '';
+let asterisk = '*';
+let spaceLine = '';
 
-for (let i = 0; i < n; i += 1) {
-  
+for (let i = 1; i < n; i += 1) {
+
   for (let j = 0; j < n; j += 1) {
-    
+    let espaco = ' ';
     if (asterisk.length < n) {
-      asterisk += '-*'
-      
+      spaceLine = spaceLine + asterisk
+      console.log(spaceLine);
     }
   }
-  console.log(asterisk)
 }
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-04.js
@@ -0,0 +1,16 @@
+// Exercicios 4 - Depois, faça uma pirâmide com n asteriscos de base:
+
+
+// n = 5
+
+//   *
+//  ***
+// *****
+
+let n = 5;
+let asterisk = '';
+
+for (let index = 0; index < n; index += 1) {
+  
+  console.log(n);
+}
