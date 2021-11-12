0ff43bdee875cfdc04d0b8fc4fa27fc4688c6bc2
Alex Silva <alexcssilva@gmail.com>
2021-11-11 23:00:27 -0300

    Parte 2 - Exercicios 6 concluido

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-objects/exercicios-02.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-objects/exercicios-02.js
@@ -3,6 +3,10 @@
 3 - Crie uma função para mostrar o tamanho de um objeto.
 4 - Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 5 - Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
+6 - Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+7 - Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
+8 - Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave. Exemplo:
+
  */
 
 const lesson1 = {
@@ -58,3 +62,19 @@ const valueObject = (obj) => obj = Object.values(obj);
 
 const allLessons = Object.assign({}, {lesson1, lesson2, lesson3})
 // console.log(allLessons);
+
+
+
+// 6 - Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+
+const returnStudents = (obj) => {
+  
+  const students = Object.keys(obj);
+  let totalStundents = 0;
+  
+  for (index in students) {
+    totalStundents += obj[students[index]].numeroEstudantes;
+  }
+  return totalStundents;
+};
+// console.log(returnStudents(allLessons));
