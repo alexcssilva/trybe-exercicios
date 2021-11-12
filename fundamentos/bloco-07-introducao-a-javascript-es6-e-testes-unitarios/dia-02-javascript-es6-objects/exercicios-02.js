98cbb25bd1487e9b3d6674e61daecd13b1966f71
Alex Silva <alexcssilva@gmail.com>
2021-11-11 23:10:33 -0300

    Parte 2 - Exercicios 7 concluido

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-objects/exercicios-02.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-objects/exercicios-02.js
@@ -6,8 +6,29 @@
 6 - Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
 7 - Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
 8 - Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave. Exemplo:
-
  */
+
+/*
+console.log(allLessons);
+
+{
+  lesson1:
+   { materia: 'Matemática',
+     numeroEstudantes: 20,
+     professor: 'Maria Clara',
+     turno: 'manhã' },
+  lesson2:
+   { materia: 'História',
+     numeroEstudantes: 20,
+     professor: 'Carlos',
+     turno: 'noite' },
+  lesson3:
+   { materia: 'Matemática',
+     numeroEstudantes: 10,
+     professor: 'Maria Clara',
+     turno: 'noite' }
+};
+*/
 
 const lesson1 = {
   materia: 'Matemática',
@@ -78,3 +99,11 @@ const returnStudents = (obj) => {
   return totalStundents;
 };
 // console.log(returnStudents(allLessons));
+
+
+// 7 - Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
+// console.log(getValueByNumber(lesson1, 0));
+// Output: 'Matématica'
+
+const valueKey = (obj, position) => Object.values(obj)[position];
+// console.log(valueKey(lesson2, 0));
