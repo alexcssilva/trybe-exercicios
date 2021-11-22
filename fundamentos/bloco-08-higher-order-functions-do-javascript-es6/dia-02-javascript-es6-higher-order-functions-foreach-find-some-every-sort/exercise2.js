023e69a7a469e0b4fa56f3b906f0d8108d03bce5
Alex Silva <alexcssilva@gmail.com>
2021-11-22 11:38:07 -0300

    Exercicios 02 concluido

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise2.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise2.js
@@ -63,12 +63,19 @@ const books = [
 
 // Adicione o código do exercício aqui:
 
-// 1 - Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
-// Dica: use a função find .
+// 2 - Retorne o nome do livro de menor nome.
+// Dica: use a função forEach .
 
-
-function authorBornIn1947() {
-  return books.find(date => date.author.birthYear === 1947).author.name;
+function smallerName() {
+  let nameBook;
+  // escreva aqui o seu código
+  books.forEach((book) => {
+    if(!nameBook || book.name.length < nameBook.length) {
+      nameBook = book.name
+    }
+  });
+  // Variável nameBook que receberá o valor do menor nome;
+  return nameBook;
 }
 
-console.log(authorBornIn1947());
+console.log(smallerName());
