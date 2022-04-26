743281e3a314f8c68fa9a8c6c5925c4be59e7bd6
Alex Silva <alexcssilva@gmail.com>
2022-04-26 16:44:30 -0300

    Exer Fixacao concluido

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodejs-um-motor-javascript/hello-world/index.js
@@ -0,0 +1,10 @@
+const readline = require('readline-sync');
+
+// console.log('Hello, world!');
+
+// const readline = require('readline-sync');
+
+const name = readline.question('Qual seu nome? ');
+const age = readline.questionInt('Qual sua idade? ');
+
+console.log(`Hello, ${name}! You are ${age} years old!`);
