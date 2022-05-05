167d1d9f6629187d2f41da923230f94e23cf5724
Alex Silva <alexcssilva@gmail.com>
2022-05-05 15:41:55 -0300

    Exercicios de Fixação

---
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/models/Users.js
@@ -0,0 +1,11 @@
+const users = require('../data/users.json');
+
+const getAll = () => {
+
+  return users;
+
+};
+
+module.exports = {
+  getAll,
+};https://ge.globo.com/futebol/playlist/entenda-tudo-sobre-a-reuniao-da-libra-a-liga-do-futebol-brasileiro.ghtml
