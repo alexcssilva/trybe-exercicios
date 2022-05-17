281fc22f4a6f782fe2106a4ed64cd08bf5b68ebf
Alex Silva <alexcssilva@gmail.com>
2022-05-17 16:06:56 -0300

    fix: Exercicios 06 e 07 concluido - BoolsController.js

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios_24.1/src/controller/BooksController.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios_24.1/src/controller/BooksController.js
@@ -6,6 +6,14 @@ const getAll = async () => {
   return books;
 };
 
+const getById = async (req, res) => {
+  const book = await BookService.getById(req.params.id);
+
+  if (!book) return res.status(404).json({ message: 'Book not found' });
+
+  res.status(200).json(book);
+};
+
 module.exports = {
   getAll,
 };
