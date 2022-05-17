c6826260ceb22b0a1b02712af25bd4ddf7a4dde0
Alex Silva <alexcssilva@gmail.com>
2022-05-17 15:50:01 -0300

    feat: added path exercicios_24.1

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios_24.1/index.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios_24.1/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const router = require('../exercicios_24.1/src/routers/index');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use(router)
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
