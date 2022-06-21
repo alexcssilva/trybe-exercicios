5d2d0c6e7149749fdb0149495e13be6c2d9fffba
Alex Silva <alexcssilva@gmail.com>
2022-06-20 21:45:08 -0300

    feat: added Exercicios 2

--- back-end/bloco-27-poo-solid/dia-01-introducao-a-orientacao-a-objetos/exercicios/Students.ts
+++ back-end/bloco-27-poo-solid/dia-01-introducao-a-orientacao-a-objetos/exercicios/Students.ts
@@ -44,10 +44,32 @@ class Students {
     }
     this._worksGrades = value;
   }
+
+  sumGrades(): number {
+    return [...this.examsGrades, ...this.worksGrades]
+    .reduce((previousGrade, grade) => {
+      grade += previousGrade;
+      return grade;
+    }, 0)
+  }
+
+  sumAverageGrades(): number {
+    const sumGrades = this.sumGrades();
+    const divider = this.examsGrades.length + this.worksGrades.length
+
+    return Math.round(sumGrades / divider);
+  }
+
 }
 
 const stundent1 = new Students('1234', 'Alex', [5, 6, 7, 8], [8, 10]);
 console.log(stundent1);
+console.log('Soma das notas: ', stundent1.sumGrades());
+console.log('Média das notas: ', stundent1.sumAverageGrades());
+
 
 const stundent2 = new Students('1234', 'Sandra', [7, 8, 10, 9], [9, 6]);
 console.log(stundent2);
+console.log('Soma das notas: ', stundent2.sumGrades());
+console.log('Média das notas: ', stundent2.sumAverageGrades());
+
