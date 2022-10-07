4ee0cf885073947a78c6efcb6ed3a9b5d6bd156c
Alex Silva <alexcssilva@gmail.com>
2022-10-06 21:20:20 -0300

    feat: added exercicios 2 concluido

---
+++ ciencia-da-computacao/bloco-36-algoritmos/dia-02-recursividade-e-estrategias-para-solucao-de-problemas/exercicios/exercicios2.py
@@ -0,0 +1,9 @@
+# Exercício 2: Transforme o algoritmo criado acima em recursivo.
+
+def conta_pares(n):
+    if n == 1:
+        return 0
+    elif n % 2 == 0:
+        return 1 + conta_pares(n-1)
+    else:
+        return conta_pares(n-1)
