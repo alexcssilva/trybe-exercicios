6d7b2821b95ab599791f57e31bdf8b85463f3802
Alex Silva <alexcssilva@gmail.com>
2022-10-07 20:58:32 -0300

    feat: added exercicios 2 concluido

---
+++ ciencia-da-computacao/bloco-36-algoritmos/dia-03-algoritmos-de-ordenacao-e-busca/exercicios/exercicios2.py
@@ -0,0 +1,10 @@
+# Exercício 2 Demonstre o passo a passo do processo de mistura de um array, sendo ordenado utilizando merge sort. Comece o passo a passo a partir da linha abaixo:
+# 7 3    5 4    6 8    2 1
+
+7    3    5    4    6    8    2    1
+
+3 7    4 5    6 8    1 2
+
+3 4 5 7    1 2 6 8
+
+1 2 3 4 5 6 7 8
